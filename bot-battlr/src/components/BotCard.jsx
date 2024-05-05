95c0bb1baa19f26b5c3ea0602cfa498bd0b0b298
Abigael <fambabigael38@gmail.com>
2024-05-05 21:55:13 +0300

    code

---
+++ bot-battlr/src/components/BotCard.jsx
@@ -0,0 +1,21 @@
+
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
+
+function BotCard({ bot }) {
+    const {id} = useParams()
+  return (
+    <div className="bot-card-1">
+      <img src={bot.avatar_url} alt={bot.name} />
+      <h3>{bot.name}</h3>
+      <p>Class: {bot.bot_class}</p>
+      <p>❤️: {bot.health}</p>
+      <p>💀: {bot.damage}</p>
+      <p>Armor: {bot.armor}</p>
+      
+    </div>
+  );
+}
+
+export default BotCard;
